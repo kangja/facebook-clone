fa85ab3ce570a2eec670c826714a1ecfabbdc8a3
Jason <kangja1996@gmail.com>
2021-08-23 00:18:45 -0400

    progress

--- components/Header.js
+++ components/Header.js
@@ -17,21 +17,21 @@ import {
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
+      <h1 className="text-6xl">Header</h1>
 
       {/* left */}
-      <div>
+      <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
           height={40}
           layout="fixed"
         />
-      </div>
 
-      <div>
-        <SearchIcon className="h-6" />
-        <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full">
+          <SearchIcon className="h-6" />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
       </div>
 
       {/* center */}
