f75209d31635992d58cad91b3efce1e973798204
Jason <kangja1996@gmail.com>
2021-08-25 14:09:01 -0400

    progress

--- components/Header.js
+++ components/Header.js
@@ -13,12 +13,11 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
     <div>
-      <h1 className="text-6xl">Header</h1>
-
       {/* left */}
       <div className="flex items-center">
         <Image
@@ -28,13 +27,26 @@ function Header() {
           layout="fixed"
         />
 
-        <div className="flex ml-2 items-center rounded-full">
-          <SearchIcon className="h-6" />
-          <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
 
       {/* center */}
+      <div className="flex justify-center flex-grow ">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* right */}
     </div>
---
+++ components/HeaderIcon.js
@@ -0,0 +1,9 @@
+function HeaderIcon({ Icon }) {
+  return (
+    <div className="cursor-pointer">
+      <Icon className="h-5 md:px-10" />
+    </div>
+  );
+}
+
+export default HeaderIcon;
