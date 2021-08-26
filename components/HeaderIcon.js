043bdc836cf6a4171d896facc637a5445dcf292f
Jason <kangja1996@gmail.com>
2021-08-26 13:17:17 -0400

    progress

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,10 +1,9 @@
 function HeaderIcon({ Icon }) {
   return (
-    <div className="cursor-pointer">
-      <Icon className="h-5 md:px-10" />
+    <div className="flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group">
+      <Icon className="h-5 text-center sm:h-7 mx-auto group-hover:text-blue-500" />
     </div>
   );
 }
 
 export default HeaderIcon;
-sdfs;
